323ef3925561a62b9fbd7a17bf3d9b36aaa60fcc
Ian Morton <IanM@Leos-MacBook-Pro-2.local>
2018-11-11 22:18:03 +0000

    sync to messageChannel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-const MESS = 'message'
+const MESS = 'messageChannel'
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
