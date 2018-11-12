32f69ec2ba937578ceb5c27885a252b92d3635ee
ian morton <ian.happytuesdays@gmail.com>
2018-11-12 19:08:28 +0000

    second message type

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 const MESS = 'messageChannel'
+const MESS2 = 'centralChannel'
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  socket.on( MESS2, function(msg){
+    io.emit( MESS2, msg);
+  });
   socket.on( MESS, function(msg){
     io.emit( MESS, msg);
   });
