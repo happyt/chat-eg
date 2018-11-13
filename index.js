268bc0cd6b669070bea21346be614f0183627b09
ian morton <ian.happytuesdays@gmail.com>
2018-11-13 18:35:58 +0000

    Simple strings version

--- index.js
+++ index.js
@@ -3,33 +3,40 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 const MESS = 'messageChannel'
-const MESS2 = 'centralChannel'
+const CMND = 'commandChannel'
 let started = false
 let pid = null
+let count = 0
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on( MESS2, function(msg){
+  console.log('connected===>')
+  socket.on( MESS, function(msg){
+    console.log('message==> ', msg)
+    io.emit( MESS, msg);
+  })
+
+  socket.on( CMND, function(msg){
+    console.log('COMMAND ', msg)
     if (msg === 'PINGo') {
       started = !started
       if (started) {
         io.emit( MESS, 'started');
         pid = setInterval(function(){
-          io.emit( MESS, 'ping');
+          count++
+          io.emit( MESS, 'ping ' + count);
           }, 3000);
       } else {
         io.emit( MESS, 'stopped');
         clearTimeout(pid);
       }
     }
-    io.emit( MESS2, msg);
+    io.emit( CMND, msg);
   });
-  socket.on( MESS, function(msg){
-    io.emit( MESS, msg);
-  });
+  
 });
 
 http.listen(port, function(){
